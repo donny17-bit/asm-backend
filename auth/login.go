b8747e587d701fae0e69388c84dd81d2e194165e
Doni Wahyu <fransiscus_budi@sinarmas.co.id>
2024-03-08 16:56:29 +0700

    make login post function

--- auth/login.go
+++ auth/login.go
@@ -2,7 +2,9 @@ package auth
 
 import (
 	"asm-backend/helper"
+	// "asm-backend/wrapper"
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -22,11 +24,105 @@ func Login(c *gin.Context) {
 
 	fmt.Println("success connecting to oracle database")
 
-	// check for error
-	err = db.Ping()
-	// check for error
+	defer db.Close()
+
+	// Ping the database to check the connection
+	if err := db.Ping(); err != nil {
+		fmt.Println("Error pinging database:", err)
+		return
+	}
+
+	// Execute a query
+	rows, err := db.Query("SELECT LUS_ID, NIK, PASS_ID FROM LST_USER_ASURANSI WHERE ROWNUM < 10")
+	// rows, err := db.QueryContext(context.Background(), "SELECT LUS_ID, NIK, PASS_ID FROM LST_USER_ASURANSI WHERE ROWNUM < 10")
+	if err != nil {
+		fmt.Println("Error executing query:", err)
+		return
+	}
+	defer rows.Close()
+
+	// Iterate through the result set
+	for rows.Next() {
+		var (
+			column1 string
+			column2 string
+			column3 string
+		)
+		if err := rows.Scan(&column1, &column2, &column3); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		// Do something with the row data
+		fmt.Println(column1, column2, column3)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error iterating rows:", err)
+		return
+	}
+}
+
+func PostLogin(c *gin.Context) {
+	// Retrieve form values
+	// case sensitive!!!!
+	nik := c.PostForm("nik")
+	password := c.PostForm("password")
+
+	db, err := helper.OraModel()
 
 	if err != nil {
-		fmt.Println("error pinging to oracle database")
+		fmt.Println("error occured when connecting to oracle database : ", err)
+		return
 	}
+
+	defer db.Close()
+
+	// Execute a query
+	rows, err := db.Query("SELECT LUS_ID, NIK, PASS_ID FROM LST_USER_ASURANSI WHERE NIK = :param1 AND PASS_ID = :param2", nik, password)
+
+	if err != nil {
+		fmt.Println("Error executing query:", err)
+		return
+	}
+	defer rows.Close()
+
+	// Iterate through the result set
+	for rows.Next() {
+		var (
+			nik       string
+			password  string
+			tgl_akhir string
+			ldi_id    string
+		)
+
+		if err := rows.Scan(&nik, &password, &tgl_akhir, &ldi_id); err != nil {
+			fmt.Println("Error scanning row:", err)
+			return
+		}
+
+		// Do something with the row data
+		fmt.Println(nik, password, tgl_akhir, ldi_id)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error iterating rows:", err)
+		return
+	}
+
+	responseData := gin.H{
+		"status": http.StatusOK,
+		"msg":    "success get data",
+		"data": []gin.H{
+			{
+				"email":    nik,
+				"password": password,
+			},
+		},
+	}
+
+	c.JSON(http.StatusOK, responseData)
+
+	// wrapper nnti aja
+	// wrapper.JsonWrapper(http.StatusOK, c, responseData)
 }
